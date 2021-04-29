825e9ef4593ce73e97aef4332b6c8175a04f3f0c
Fernández Micaelai <porokawaiilol@gmail.com>
2021-04-29 19:17:39 -0300

    Codigo de clase solucionado, falta mejorar nuevamente css

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,25 +28,13 @@ const Card = personaje => {
 }
 
 const Modal = personaje => {
-    const {id, name, status, image, species, episode} = personaje;
-
-    const episodeGrid = episode.map(async (episodeItem) => {
-        const episodeData = await getEpisode(episodeItem);
-        console.log(episodeData);
-        const {name, air_date} = episodeData;
-        console.log(name);
-        return `<li>${name}</li>`;
-
+    console.log(personaje)
+    const { id, name, status, species, image, episodesData } = personaje
+    //recorro todos los episodios y los voy acumulando en episodesLi
+    let episodesLi = ''
+    episodesData.forEach(({ name })=>{
+        episodesLi += `<li>${name}</li>`
     })
-console.log('Grid', episodeGrid)
-
-    //episode.forEach(async (episodeItem) => {
-      //  const episodeData = await getEpisode(episodeItem);
-        //console.log(episodeData);
-
-        //Armar grilla de contenido de episodios.
-
-    //})
 
 
     return `<div class="box">
@@ -66,7 +54,7 @@ console.log('Grid', episodeGrid)
                             Especie: ${species}.
                             </p>
                             <h3>Episodes</h3>
-                            <ul>${episodeGrid}</ul>
+                            <ul>${episodesLi}</ul>
                         </div>
                         <nav class="level is-mobile">
                             <div class="level-left">
@@ -91,99 +79,72 @@ console.log('Grid', episodeGrid)
 //-------------------- CODE --------------------
 
 
-const appendElements = (characters, clearGrid = false) => {
-
+const appendElements = (characters, borrarGrilla = false) => {
     const $grid = document.querySelector('.grid');
-    if (clearGrid){
-        $grid.innerHTML = '';
+    if (borrarGrilla) {
+        $grid.innerHTML = null;
     }
-
     characters.forEach(character => {
         const cardItem = Card(character);
-        $grid.innerHTML += cardItem; 
+        $grid.innerHTML += cardItem;
     });
-
     const $modalOpenArr = document.querySelectorAll('.handleOpenModal');
     const $modal = document.querySelector('.modal');
     const $modalContent = document.querySelector('.modal-content');
     const $modalClose = document.querySelector('.modal-close');
-
-    $modalClose.addEventListener('click', () =>{
+    $modalClose.addEventListener('click', () => {
         $modal.classList.remove('is-active');
     })
-
     $modalOpenArr.forEach(($card) => {
-        $card.addEventListener('click', async () => {
+        $card.addEventListener('click', () => {
             const id = $card.dataset.id;
-            const character = characters[id - 1];
-            $modalContent.innerHTML = Modal(character);
-            $modal.classList.add('is-active');
-        });
+            const character = characters[id - 1]; //Nos da la posicion del personaje en el array de characters
+            const { episode } = character
+            const getEpisodesData = async () => {
+                return Promise.all(episode.map(item => getEpisode(item))) //Resuelvo cada una de las promesas (fetchs de episodes)
+            }
+            getEpisodesData().then(episodesData => {
+                const characterWithEpisodes = { ...character, episodesData } //Junto los datos que tenia del character + sus episodes
+                $modalContent.innerHTML = Modal(characterWithEpisodes) //Le mando todo junto a modal
+                $modal.classList.add('is-active'); //Activo el modal
+            })
+        })
     })
 }
 
 
-const getCharacters = async (baseUrl, from, to) => {
-
-    const charactersRange = Array.from({ length: to - from + 1 }, (_, index) => index + 1).join(',');//Va a hacer un for con el array de la API.
-    const url = `${baseUrl}/character/${charactersRange}`;
+const getCharacters = async (baseURL, from, to) => {
+    const charactersRange = Array.from({ length: to - from + 1 }, (_, index) => index + 1).join(',');
+    const url = `${baseURL}character/${charactersRange}`;
     const response = await fetch(url);
     const characters = await response.json();
-
     return characters;
-
 }
-
-const getCharacter = async (baseUrl, id) => {
-
-    const url = `${baseUrl}/character/${id}`;
-    const response = await fetch(url);
-    const character = await response.json();
-
-    return character;
-
-}
-
-const getEpisode = async (baseUrl) => {
-
-    const url = `${baseUrl}`;
+const getEpisode = async (baseURL) => {
+    const url = `${baseURL}`;
     const response = await fetch(url);
     const episode = await response.json();
-
     return episode;
-
 }
-
-const getCharactersByQuery = async (baseUrl, searchValue) => {
-    const url = `${baseUrl}/character/?name=${searchValue}`;
+const getCharactersByQuery = async (baseURL, valorABuscar) => {
+    const url = `${baseURL}character/?name=${valorABuscar}`;
     const response = await fetch(url);
     const characters = await response.json();
-
     return characters;
-
 }
 
 const main = async () => {
-
-    const baseUrl= 'https://rickandmortyapi.com/api';
-    const characters = await getCharacters(baseUrl, 1, 20);
-    appendElements(characters);
-
+    const baseURL = 'https://rickandmortyapi.com/api/';
+    const characters = await getCharacters(baseURL, 1, 20);
+    appendElements(characters)
     const $submit = document.querySelector('.handle_search');
     $submit.addEventListener('click', async (event) => {
-        console.log('FUNCA');
-        event.preventDefault();//Hace que no me recargue la pagina cuando hago click.
-        const $input = document.querySelector('.input_search');
-        const value = $input.value;//Esta constante es lo que voy a pasar de segundo parametro abajo, es lo que va a tener los nombres.
-
-        const charactersByQuery = await getCharactersByQuery(baseUrl, value);
+        event.preventDefault();
+        const $input = document.querySelector('.input_search')
+        const value = $input.value;
+        const charactersByQuery = await getCharactersByQuery(baseURL, value)
         const characters = charactersByQuery.results;
-
         appendElements(characters, true);
-        console.log(charactersByQuery.results);
-
     })
-
 }
-
 main();
