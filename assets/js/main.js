e12738704ec52a608cc32ce510cce152208a8eba
Fernández Micaelai <porokawaiilol@gmail.com>
2021-04-29 12:50:14 -0300

    agregado todo lo hecho en clase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 const Card = personaje => {
-    const {name, status, image, species} = personaje;
+    const {id, name, status, image, species} = personaje;
 
     return `<div class="column is-one-quarter-desktop is-half-tablet is-full-mobile">
-                <div class="card">
+                <a class= "handleOpenModal" data-id = ${id}><div class="card">
                     <div class="card-image">
                         <figure class="image is-4by3">
                             <img src="${image}" alt="Placeholder image">
@@ -22,18 +22,104 @@ const Card = personaje => {
                             <p>${status}</p>
                         </div>
                     </div>
-                </div>
+                </div></a>
             </div>`
 
 }
 
-const appendElements = (characters) => {
+const Modal = personaje => {
+    const {id, name, status, image, species, episode} = personaje;
+
+    const episodeGrid = episode.map(async (episodeItem) => {
+        const episodeData = await getEpisode(episodeItem);
+        console.log(episodeData);
+        const {name, air_date} = episodeData;
+        console.log(name);
+        return `<li>${name}</li>`;
+
+    })
+console.log('Grid', episodeGrid)
+
+    //episode.forEach(async (episodeItem) => {
+      //  const episodeData = await getEpisode(episodeItem);
+        //console.log(episodeData);
+
+        //Armar grilla de contenido de episodios.
+
+    //})
+
+
+    return `<div class="box">
+                <article class="media">
+                    <div class="media-left">
+                        <figure class="image is-64x64">
+                            <img src="${image}" alt="Image">
+                        </figure>
+                    </div>
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                            <strong>${name}</strong>
+                            <br>
+                            Status: ${status}.
+                            <br>
+                            Especie: ${species}.
+                            </p>
+                            <h3>Episodes</h3>
+                            <ul>${episodeGrid}</ul>
+                        </div>
+                        <nav class="level is-mobile">
+                            <div class="level-left">
+                                <a class="level-item" aria-label="retweet">
+                                    <span class="icon is-small">
+                                        <i class="fas fa-retweet" aria-hidden="true"></i>
+                                    </span>
+                                </a>
+                                <a class="level-item" aria-label="like">
+                                    <span class="icon is-small">
+                                        <i class="fas fa-heart" aria-hidden="true"></i>
+                                    </span>
+                                </a>
+                            </div>
+                        </nav>
+                    </div>
+                </article>
+            </div>`
+}
+
+
+//-------------------- CODE --------------------
+
+
+const appendElements = (characters, clearGrid = false) => {
 
     const $grid = document.querySelector('.grid');
+    if (clearGrid){
+        $grid.innerHTML = '';
+    }
+
     characters.forEach(character => {
         const cardItem = Card(character);
         $grid.innerHTML += cardItem; 
     });
+
+    const $modalOpenArr = document.querySelectorAll('.handleOpenModal');
+    const $modal = document.querySelector('.modal');
+    const $modalContent = document.querySelector('.modal-content');
+    const $modalClose = document.querySelector('.modal-close');
+
+    $modalClose.addEventListener('click', () =>{
+        $modal.classList.remove('is-active');
+    })
+
+    $modalOpenArr.forEach(($card) => {
+        $card.addEventListener('click', async () => {
+            const id = $card.dataset.id;
+            const character = characters[id - 1];
+            $modalContent.innerHTML = Modal(character);
+            $modal.classList.add('is-active');
+        });
+    })
 }
 
 
@@ -48,6 +134,34 @@ const getCharacters = async (baseUrl, from, to) => {
 
 }
 
+const getCharacter = async (baseUrl, id) => {
+
+    const url = `${baseUrl}/character/${id}`;
+    const response = await fetch(url);
+    const character = await response.json();
+
+    return character;
+
+}
+
+const getEpisode = async (baseUrl) => {
+
+    const url = `${baseUrl}`;
+    const response = await fetch(url);
+    const episode = await response.json();
+
+    return episode;
+
+}
+
+const getCharactersByQuery = async (baseUrl, searchValue) => {
+    const url = `${baseUrl}/character/?name=${searchValue}`;
+    const response = await fetch(url);
+    const characters = await response.json();
+
+    return characters;
+
+}
 
 const main = async () => {
 
@@ -55,6 +169,21 @@ const main = async () => {
     const characters = await getCharacters(baseUrl, 1, 20);
     appendElements(characters);
 
+    const $submit = document.querySelector('.handle_search');
+    $submit.addEventListener('click', async (event) => {
+        console.log('FUNCA');
+        event.preventDefault();//Hace que no me recargue la pagina cuando hago click.
+        const $input = document.querySelector('.input_search');
+        const value = $input.value;//Esta constante es lo que voy a pasar de segundo parametro abajo, es lo que va a tener los nombres.
+
+        const charactersByQuery = await getCharactersByQuery(baseUrl, value);
+        const characters = charactersByQuery.results;
+
+        appendElements(characters, true);
+        console.log(charactersByQuery.results);
+
+    })
+
 }
 
-main()
+main();
